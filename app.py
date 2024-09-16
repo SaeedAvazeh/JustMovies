0988bb1252ef104eb91f563fe8bf787ca77b4169
SaeedAvazeh <125880779+SaeedAvazeh@users.noreply.github.com>
2024-09-16 10:55:27 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask import SQLAlchemy
+from flask import Bcrypt
+from flask import LoginManager
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
